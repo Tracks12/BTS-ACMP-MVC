df539e2b3fd221a2a496313c311e99a7d37a2f70
tracks12 <anarchy.dark97@gmail.com>
2020-05-05 02:35:23 +0200

    Création de nouveaux models et ajouts nouvelles jauges
    Co-Authored-By: Flopicx <flowsheik2@gmail.com>

--- core/models/ACMPModel.php
+++ core/models/ACMPModel.php
@@ -21,6 +21,17 @@
 			return $req->fetchAll(PDO::FETCH_ASSOC);
 		}
 
+		public function getMeasuresName(): array {
+			$bdd = bdd::connect();
+			$req = $bdd->query('
+				SELECT *
+				FROM `measuresName`
+				WHERE 1
+			');
+
+			return $req->fetchAll(PDO::FETCH_ASSOC);
+		}
+
 		/**
 		 * get return of datas by captors
 		 * @return array datas by captors
@@ -42,21 +53,42 @@
 		}
 
 		/**
+		 * get return of datas of one captors
+		 * @param string $captor captor name
+		 * @return array datas of captor
+		 */
+		public function getDataByOnceCaptor(string $captor): array {
+			$bdd = bdd::connect();
+			$req = $bdd->query("
+				SELECT `id`, `Name`, `value`, `rssi`, `lat`, `lon`, `time`
+				FROM `data`
+				JOIN `captors` ON `captors`.idCaptor = `data`.idCaptor
+				JOIN `measures` ON `measures`.idMeasure = `data`.idMeasure
+				JOIN `measuresName` ON `measuresName`.idName = `measures`.idName
+				WHERE `captors`.id = '$captor'
+				ORDER BY `measures`.idMeasure
+				DESC
+			");
+
+			return $req->fetchAll(PDO::FETCH_ASSOC);
+		}
+
+		/**
 		 * get last value for a physic measure
 		 * @param string $measureName measure name
 		 * @return array last value
 		 */
-		public function getLastValueFor($measureName): array {
+		public function getLastValueFor(string $measureName): array {
 			$bdd = bdd::connect();
-			$req = $bdd->query('
+			$req = $bdd->query("
 				SELECT `Name`, `value`
 				FROM `measures`
 				JOIN `measuresName` ON `measures`.idName = `measuresName`.idName
-				WHERE `measuresName`.Name = "CO2"
+				WHERE `measuresName`.Name = '$measureName'
 				ORDER BY `measures`.idMeasure
 				DESC
 				LIMIT 1
-			');
+			");
 
 			return $req->fetchAll(PDO::FETCH_ASSOC)[0];
 		}
--- core/views/telemetry/instant.php
+++ core/views/telemetry/instant.php
@@ -5,10 +5,31 @@
 <!DOCTYPE html>
 <!-- INSTANT -->
 <aside id="instant">
-	<h2>temps réel</h2>
 	<br />
 	<figure class="highcharts-figure">
-		<div id="container-speed" class="chart-container"></div>
+		<div id="carbon" class="chart-container"></div>
+		<p class="highcharts-description">
+			Chart demonstrating solid gauges with dynamic data. Two separate charts
+			are used, and each is updated dynamically every few seconds. Solid
+			gauges are popular charts for dashboards, as they visualize a number
+			in a range at a glance. As demonstrated by these charts, the color of
+			the gauge can change depending on the value of the data shown.
+		</p>
+	</figure>
+	<br />
+	<figure class="highcharts-figure">
+		<div id="ozonne" class="chart-container"></div>
+		<p class="highcharts-description">
+			Chart demonstrating solid gauges with dynamic data. Two separate charts
+			are used, and each is updated dynamically every few seconds. Solid
+			gauges are popular charts for dashboards, as they visualize a number
+			in a range at a glance. As demonstrated by these charts, the color of
+			the gauge can change depending on the value of the data shown.
+		</p>
+	</figure>
+	<br />
+	<figure class="highcharts-figure">
+		<div id="particules" class="chart-container"></div>
 		<p class="highcharts-description">
 			Chart demonstrating solid gauges with dynamic data. Two separate charts
 			are used, and each is updated dynamically every few seconds. Solid
@@ -19,12 +40,12 @@
 	</figure>
 	<script type="text/javascript">
 		// CO2
-		var chartSpeed = Highcharts.chart('container-speed', Highcharts.merge(gaugeOptions, {
+		var carbon = Highcharts.chart('carbon', Highcharts.merge(gaugeOptions, {
 		  yAxis: {
 	      min: 0,
 	      max: 100000,
 	      title: {
-          text: 'Speed'
+          text: 'CO2'
 	      }
 		  },
 
@@ -33,8 +54,68 @@
 		  },
 
 		  series: [{
-	      name: 'Speed',
+	      name: 'CO2',
 	      data: [<?php echo(ACMPModel::getLastValueFor('CO2')['value']); ?>],
+	      dataLabels: {
+          format:
+            '<div style="text-align:center">' +
+            '<span style="font-size:25px">{y}</span><br/>' +
+            '<span style="font-size:12px;opacity:0.4">ppm</span>' +
+            '</div>'
+	      },
+	      tooltip: {
+          valueSuffix: ' ppm'
+	      }
+		  }]
+		}));
+
+		// Ozone
+		var ozone = Highcharts.chart('ozonne', Highcharts.merge(gaugeOptions, {
+		  yAxis: {
+	      min: 0,
+	      max: 200,
+	      title: {
+          text: 'Ozone'
+	      }
+		  },
+
+		  credits: {
+	      enabled: false
+		  },
+
+		  series: [{
+	      name: 'Ozone',
+	      data: [<?php echo(ACMPModel::getLastValueFor('Ozonne')['value']); ?>],
+	      dataLabels: {
+          format:
+            '<div style="text-align:center">' +
+            '<span style="font-size:25px">{y}</span><br/>' +
+            '<span style="font-size:12px;opacity:0.4">dobson</span>' +
+            '</div>'
+	      },
+	      tooltip: {
+          valueSuffix: ' dobson'
+	      }
+		  }]
+		}));
+
+		// Particules Fines
+		var particules = Highcharts.chart('particules', Highcharts.merge(gaugeOptions, {
+		  yAxis: {
+	      min: 0,
+	      max: 200,
+	      title: {
+          text: 'Particules Fines'
+	      }
+		  },
+
+		  credits: {
+	      enabled: false
+		  },
+
+		  series: [{
+	      name: 'Particules Fines',
+	      data: [<?php echo(ACMPModel::getLastValueFor('Particules Fines')['value']); ?>],
 	      dataLabels: {
           format:
             '<div style="text-align:center">' +
@@ -55,8 +136,14 @@
 		  // CO2 Update
 		  var point;
 
-		  if(chartSpeed)
-				xhr.getLastValueFor(chartSpeed.series[0].points[0], 'CO2');
+		  if(carbon)
+				xhr.getLastValueFor(carbon.series[0].points[0], 'CO2');
+
+			if(ozone)
+				xhr.getLastValueFor(ozone.series[0].points[0], 'Ozonne');
+
+			if(particules)
+				xhr.getLastValueFor(particules.series[0].points[0], 'Particules Fines');
 		}, 2000);
 	</script>
 </aside>
