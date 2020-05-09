91ce21c46fccf5b3f81aaa7af4779bcb405fc29f
tracks12 <anarchy.dark97@gmail.com>
2020-05-09 05:55:06 +0200

    MàJ BDD (Procédures & Fonctions pour administration d'utilisateurs)

--- capteur.sql
+++ capteur.sql
@@ -83,13 +83,17 @@ INSERT INTO `measuresName` (`idName`, `name`, `unit`) VALUES
 -- --------------------------------------------------------
 
 CREATE TABLE `users` (
-	`idUser` int(11) NOT NULL,
-	`nichandle` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
-	`password` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-	`lastConnect` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	`lastAddr` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL,
-	`isAdmin` tinyint(1) NOT NULL DEFAULT '0'
+	`idUser` int(11) NOT NULL COMMENT '[int(11)]',
+	`nichandle` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '[varchar(32)]',
+	`mail` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '[varchar(255)]',
+	`password` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '[varchar(64)]',
+	`lastConnect` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '[timestamp]',
+	`lastAddr` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '[varchar(16)]',
+	`isAdmin` tinyint(1) NOT NULL DEFAULT '0' COMMENT '[bool(1)]'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `users` (`idUser`, `nichandle`, `mail`, `password`, `lastConnect`, `lastAddr`, `isAdmin`) VALUES
+	(1, 'admin', '', 'LJEgnJ4=', '2020-05-09 03:38:14', '', 1);
 
 
 -- Index des tables exportées
@@ -114,7 +118,8 @@ ALTER TABLE `measuresName`
 
 ALTER TABLE `users`
 	ADD PRIMARY KEY (`idUser`),
-	ADD UNIQUE KEY `nichandle` (`nichandle`);
+	ADD UNIQUE KEY `nichandle` (`nichandle`),
+	ADD UNIQUE KEY `mail` (`mail`);
 
 
 -- AUTO_INCREMENT des tables
@@ -145,9 +150,9 @@ ALTER TABLE `measures`
 	ADD CONSTRAINT `FK_NAME` FOREIGN KEY (`idName`) REFERENCES `measuresName` (`idName`);
 
 
--- Procédures
-
 DELIMITER $$
+
+-- Procédures
 
 -- GetLastCarbonData
 CREATE DEFINER=`acmp`@`localhost` PROCEDURE `GetLastCarbonData` ()  NO SQL
@@ -227,7 +232,7 @@ DESC$$
 
 -- listDataByCaptor
 CREATE DEFINER=`acmp`@`localhost` PROCEDURE `listDataByCaptor` ()  NO SQL
-SELECT `id`, `name`, `unit`, SELECT `id`, `name`, `unit`, CASE
+SELECT `id`, `name`, `unit`, CASE
 		WHEN `name` = "CO2" THEN `value` / 100
 		ELSE `value`
 	END AS `value`, `rssi`, `lat`, `lon`, `time`
@@ -240,7 +245,7 @@ ORDER BY `measures`.idMeasure
 DESC$$
 
 -- listDataByOnceCaptor
-CREATE DEFINER=`acmp`@`localhost` PROCEDURE `listDataByOnceCaptor` (IN `captor` VARCHAR(128) CHARSET utf8) NO SQL
+CREATE DEFINER=`acmp`@`localhost` PROCEDURE `listDataByOnceCaptor` (IN `captor` VARCHAR(128) CHARSET utf8)  NO SQL
 SELECT `id`, `name`, `unit`, CASE
 		WHEN `name` = "CO2" THEN `value` / 100
 		ELSE `value`
@@ -254,8 +259,9 @@ ORDER BY `measures`.idMeasure
 DESC$$
 
 -- listLastDataByCaptor
-CREATE DEFINER=`acmp`@`localhost` PROCEDURE `listLastDataByCaptor` () NO SQL
-SELECT `id`, `name`, `unit`, CASE
+CREATE DEFINER=`acmp`@`localhost` PROCEDURE `listLastDataByCaptor` ()  NO SQL
+SELECT `id`, `name`, `unit`,
+	CASE
 		WHEN `name` = "CO2" THEN `value` / 100
 		ELSE `value`
 	END AS `value`, `rssi`, `lat`, `lon`, `time`
@@ -281,6 +287,92 @@ WHERE `measures`.idMeasure IN (
 )
 ORDER BY `measures`.idMeasure
 DESC$$
+
+-- InsertNewUser
+CREATE DEFINER=`acmp`@`localhost` PROCEDURE `InsertNewUser` (IN `nichandle` VARCHAR(32) CHARSET utf8mb4, IN `mail` VARCHAR(255) CHARSET utf8mb4, IN `pass` VARCHAR(64) CHARSET utf8mb4, IN `admin` BOOLEAN)  NO SQL
+	COMMENT 'Insert new user profile in users table'
+INSERT INTO `users`(`nichandle`, `mail`, `password`, `lastAddr`, `isAdmin`) VALUES
+	(nichandle, mail, ROT13(TO_BASE64(pass)), '', admin)$$
+
+-- UpdatePasswordUser
+CREATE DEFINER=`acmp`@`localhost` PROCEDURE `UpdatePasswordUser` (IN `id` INT(11), IN `pass` VARCHAR(64) CHARSET utf8mb4)  NO SQL
+UPDATE `users`
+SET `password`= ROT13(TO_BASE64(pass))
+WHERE `idUser` = id$$
+
+
+-- Fonctions
+
+-- ROT13 Tranlator
+CREATE DEFINER=`acmp`@`localhost` FUNCTION `ROT13` (`stringIn` VARCHAR(500) CHARSET utf8mb4) RETURNS VARCHAR(500) CHARSET utf8mb4 NO SQL
+    COMMENT 'convert string to rot13'
+BEGIN
+	DECLARE v1 INT DEFAULT 1;
+	DECLARE stringOut VARCHAR(200) DEFAULT '';
+	DECLARE str VARCHAR(1) DEFAULT '';
+
+	WHILE v1 <= LENGTH(stringIn) DO
+		SET str = SUBSTR(stringIn,v1,1);
+		CASE BINARY str
+			WHEN 'A' THEN SET stringOut = CONCAT(stringOut, 'N');
+			WHEN 'a' THEN SET stringOut = CONCAT(stringOut, 'n');
+			WHEN 'B' THEN SET stringOut = CONCAT(stringOut, 'O');
+			WHEN 'b' THEN SET stringOut = CONCAT(stringOut, 'o');
+			WHEN 'C' THEN SET stringOut = CONCAT(stringOut, 'P');
+			WHEN 'c' THEN SET stringOut = CONCAT(stringOut, 'p');
+			WHEN 'D' THEN SET stringOut = CONCAT(stringOut, 'Q');
+			WHEN 'd' THEN SET stringOut = CONCAT(stringOut, 'q');
+			WHEN 'E' THEN SET stringOut = CONCAT(stringOut, 'R');
+			WHEN 'e' THEN SET stringOut = CONCAT(stringOut, 'r');
+			WHEN 'F' THEN SET stringOut = CONCAT(stringOut, 'S');
+			WHEN 'f' THEN SET stringOut = CONCAT(stringOut, 's');
+			WHEN 'G' THEN SET stringOut = CONCAT(stringOut, 'T');
+			WHEN 'g' THEN SET stringOut = CONCAT(stringOut, 't');
+			WHEN 'H' THEN SET stringOut = CONCAT(stringOut, 'U');
+			WHEN 'h' THEN SET stringOut = CONCAT(stringOut, 'u');
+			WHEN 'I' THEN SET stringOut = CONCAT(stringOut, 'V');
+			WHEN 'i' THEN SET stringOut = CONCAT(stringOut, 'v');
+			WHEN 'J' THEN SET stringOut = CONCAT(stringOut, 'W');
+			WHEN 'j' THEN SET stringOut = CONCAT(stringOut, 'w');
+			WHEN 'K' THEN SET stringOut = CONCAT(stringOut, 'X');
+			WHEN 'k' THEN SET stringOut = CONCAT(stringOut, 'x');
+			WHEN 'L' THEN SET stringOut = CONCAT(stringOut, 'Y');
+			WHEN 'l' THEN SET stringOut = CONCAT(stringOut, 'y');
+			WHEN 'M' THEN SET stringOut = CONCAT(stringOut, 'Z');
+			WHEN 'm' THEN SET stringOut = CONCAT(stringOut, 'z');
+			WHEN 'N' THEN SET stringOut = CONCAT(stringOut, 'A');
+			WHEN 'n' THEN SET stringOut = CONCAT(stringOut, 'a');
+			WHEN 'O' THEN SET stringOut = CONCAT(stringOut, 'B');
+			WHEN 'o' THEN SET stringOut = CONCAT(stringOut, 'b');
+			WHEN 'P' THEN SET stringOut = CONCAT(stringOut, 'C');
+			WHEN 'p' THEN SET stringOut = CONCAT(stringOut, 'c');
+			WHEN 'Q' THEN SET stringOut = CONCAT(stringOut, 'D');
+			WHEN 'q' THEN SET stringOut = CONCAT(stringOut, 'd');
+			WHEN 'R' THEN SET stringOut = CONCAT(stringOut, 'E');
+			WHEN 'r' THEN SET stringOut = CONCAT(stringOut, 'e');
+			WHEN 'S' THEN SET stringOut = CONCAT(stringOut, 'F');
+			WHEN 's' THEN SET stringOut = CONCAT(stringOut, 'f');
+			WHEN 'T' THEN SET stringOut = CONCAT(stringOut, 'G');
+			WHEN 't' THEN SET stringOut = CONCAT(stringOut, 'g');
+			WHEN 'U' THEN SET stringOut = CONCAT(stringOut, 'H');
+			WHEN 'u' THEN SET stringOut = CONCAT(stringOut, 'h');
+			WHEN 'V' THEN SET stringOut = CONCAT(stringOut, 'I');
+			WHEN 'v' THEN SET stringOut = CONCAT(stringOut, 'i');
+			WHEN 'W' THEN SET stringOut = CONCAT(stringOut, 'J');
+			WHEN 'w' THEN SET stringOut = CONCAT(stringOut, 'j');
+			WHEN 'X' THEN SET stringOut = CONCAT(stringOut, 'K');
+			WHEN 'x' THEN SET stringOut = CONCAT(stringOut, 'k');
+			WHEN 'Y' THEN SET stringOut = CONCAT(stringOut, 'L');
+			WHEN 'y' THEN SET stringOut = CONCAT(stringOut, 'l');
+			WHEN 'Z' THEN SET stringOut = CONCAT(stringOut, 'M');
+			WHEN 'z' THEN SET stringOut = CONCAT(stringOut, 'm');
+            WHEN str THEN SET stringOut = CONCAT(stringOut, str);
+		END CASE;
+		SET v1 = v1 + 1;
+	END WHILE;
+
+	RETURN stringOut;
+END$$
 
 DELIMITER ;
 
