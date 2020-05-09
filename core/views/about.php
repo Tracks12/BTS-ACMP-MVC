497076338efc934b0a6a665d1f814fc134800e5a
tracks12 <anarchy.dark97@gmail.com>
2020-05-09 19:51:41 +0200

    Upgrade About Animation

--- core/views/about.php
+++ core/views/about.php
@@ -7,18 +7,25 @@
 			<h1>acmp</h1>
 		</div>
 	</header>
+	<article>
+		
+	</article>
 </aside>
 <?php require_once('./core/views/footer.html'); ?>
 <script language="javascript" type="text/javascript">
 	$('#about').ready(() => {
-		$('#about > header > .title > .pic').animate({
-			opacity: 1,
-			marginBottom: "40px"
-		}, 1000);
+		$('#about > header > .title > hr').animate({
+			width: '75%'
+		}, 1000, () => {
+			$('#about > header > .title > .pic').animate({
+				opacity: 1,
+				marginBottom: "40px"
+			}, 1000);
 
-		$('#about > header > .title > h1').animate({
-			opacity: 1,
-			marginTop: "40px"
-		}, 1000);
+			$('#about > header > .title > h1').animate({
+				opacity: 1,
+				marginTop: "40px"
+			}, 1000);
+		});
 	});
 </script>
