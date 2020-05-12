e466deba27987381b13718d3c9a95a2f25c32ade
MENEGHE <meneghinieric@gmail.com>
2020-05-12 11:36:41 +0200

    Mise à jour le page historique pour affichage des 3 graph

--- core/views/telemetry/story.php
+++ core/views/telemetry/story.php
@@ -2,84 +2,97 @@
 <aside id="story">
 	<h2>story</h2>
 	<article>
-		<!--(texte expliquant comment fonctionne ce graphique)-->
-		<h5>Futur graphique a particules fines</h5>
-		<p>
-			On désigne par le terme « particule » un ensemble de substances particulaires microscopiques.</br>
-			Ces substances sont solides ou liquides et restent en suspension dans l’air en général de quelques jours à quelques années.</br>
-			Le mot « particule » englobe un ensemble de composés variés.</br>
-			Plus les particules sont petites, plus elles pénètrent facilement dans nos poumons.</br>
-			Certains types de particules semblent plus nuisibles que d’autres : c’est le cas, par exemple, de celles issues de la combustion (suies) du charbon, du diesel et du bois.</br>
-			Elles sont alors d’autant plus nocives pour notre santé ou l’environnement...
-		</p>
+		<h5>Historique</h5>
+		<p>Cette page permet de visualiser toutes les mesures effectués par les trois capteurs.</p>
 	</article>
 	<br />
 	<?php
 		//Lancement requete
-		$data = ACMPModel::getDataByOnceCaptor('3100000000000003');
+		$data = [
+			ACMPModel::getDataByOnceCaptor('3100000000000001'),
+			ACMPModel::getDataByOnceCaptor('3100000000000002'),
+			ACMPModel::getDataByOnceCaptor('3100000000000003')
+		];
 
 		//Traitement du resultat
-		$listeChart = [];
+		$listeChart = [
+			[
+				"color" => "red",
+				"name"  => NULL,
+				"unit"  => NULL,
+				"data"  => []
+			],
+			[
+				"color" => "green",
+				"name"  => NULL,
+				"unit"  => NULL,
+				"data"  => []
+			],
+			[
+				"color" => "blue",
+				"name"  => NULL,
+				"unit"  => NULL,
+				"data"  => []
+			]
+		];
 
-		foreach($data as $item) { // Fetching de la requête
-			// strtotime = conversion datetime vers timestamp
-			// intval = conversion string to integer
-			$name = $item['name'];                   // Nom de la grandeur
-			$unit = $item['unit'];                   // Unité de mesure
+		for($i = 0; $i < count($data); $i++) {
+			foreach($data[$i] as $item) { // Fetching de la requête
+				// strtotime = conversion datetime vers timestamp
+				// intval = conversion string to integer
+				$listeChart[$i]["name"] = $item['name']; // Nom de la grandeur
+				$listeChart[$i]["unit"] = $item['unit']; // Unité de mesure
 
-			array_push($listeChart, [          //format data pour highchart [x,y],[x,y]...
-				strtotime($item['time']) * 1000, // date MySQL * 1000 pour résultat en ms
-				intval($item['value'])           // Données du capteurs
-			]);
+				array_push($listeChart[$i]["data"], [    //format data pour highchart [x,y],[x,y]...
+					strtotime($item['time']) * 1000,       // date MySQL * 1000 pour résultat en ms
+					intval($item['value'])                 // Données du capteurs
+				]);
+			}
 		}
 	?>
 
 	<!-- balise pour l'affichage du graph -->
-	<div id="chart1" style="width:100%; height:400px;"></div>
+	<div id="chart1" style="width: 100%; height: 400px;"></div>
 
 	<script type="text/javascript">
-		$(() => {	// fonction en javascript
-			chart1 = new Highcharts.Chart({	// Créer un object HighChart en JS
+		$(() => { // fonction en javascript
+			chart1 = new Highcharts.Chart({ // Créer un object HighChart en JS
 				chart: {
-					renderTo: 'chart1',	// Sélectionner la balise html pour l'afficher
+					renderTo: 'chart1', // Sélectionner la balise html pour l'afficher
 					type: 'spline',
-					backgroundColor: null,
+					backgroundColor: null
 				},
 
 				title: {
-					text: '<?php echo($name); ?>',
+					text: '<?php echo("{$listeChart[0]["name"]}, {$listeChart[1]["name"]}, {$listeChart[2]["name"]}"); ?>',
 					style:{
-						color: '#4572A7',
-					},
+						color: '#4572A7'
+					}
 				},
 
-				legend: {	// La légende
+				legend: { // La légende
 					enabled: true,
 					backgroundColor: 'white',
-					borderRadius: 14,
+					borderRadius: 14
 				},
 
 				xAxis: {
 					type: 'datetime',
 					dateTimeLabelFormats: {
-						month: '%e. %b',	// Formatage sur le mois
-						year: '%b',	// Formatage sur l'année
+						month: '%e. %b', // Formatage sur le mois
+						year: '%b' // Formatage sur l'année
 					}
-				 },
+				},
 
 				yAxis: [{
+					<?php for($i = 0; $i < count($listeChart); $i++) { ?>
 					labels: {
-						format: '{value} <?php echo($unit); ?>',
+						format: '{value} <?php echo($listeChart[$i]["unit"]); ?>',
 						style: {
-							color: '#C03000',
-						},
+							color: '#C03000'
+						}
 					},
-					title: {
-						text: '',
-						style: {
-							color: '#C03000',
-						},
-					}
+					<?php } ?>
 				}],
 
 				tooltip: {
@@ -88,23 +101,27 @@
 					borderRadius: 6,
 					borderWidth: 3,
 					xDateFormat: '%A %e %b  %H:%M:%S',
-					valueSuffix: ' <?php echo($unit); ?>',
-				 },
+					valueSuffix: ''
+				},
 
 				plotOptions: {
 					spline: {
 						marker: {
-							enabled: false,
-						},
-					},
+							enabled: false
+						}
+					}
 				},
 
-				series: [{
-					name: '<?php echo($name); ?> (<?php echo($unit); ?>)',
-					color: 'red',
-					zIndex: 1,
-					data: <?php echo(json_encode($listeChart)); ?> // c'est ici qu'on insert les data
-				}]
+				series: [
+					<?php for($i = 0; $i < count($listeChart); $i++) { ?>
+					{
+						name: '<?php echo("{$listeChart[$i]["name"]} ({$listeChart[$i]["unit"]})"); ?>',
+						color: '<?php echo($listeChart[$i]["color"]); ?>',
+						zIndex: 1,
+						data: <?php echo(json_encode($listeChart[$i]["data"])); ?> // c'est ici qu'on insert les data
+					},
+					<?php } ?>
+				]
 			});
 		});
 	</script>
